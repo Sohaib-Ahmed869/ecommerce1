05a7fe7bc58a6271464ab7cb4d0090185d2ddb26
Sohaib Ahmed <118601382+Sohaib-Ahmed869@users.noreply.github.com>
2024-05-17 17:07:33 +0500

    Update db.js

--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -4,8 +4,17 @@ import mongoose from "mongoose";
 async function connectToDb() {
   if (mongoose.connections[0].readyState) return; // Use existing database connection if it exists
   // Connect to MongoDB
-    await mongoose.connect("mongodb://127.0.0.1:27017/EcommerceTEABC");
-  //   await mongoose.connect("mongodb+srv://i211114:pakistanzindabad@cluster0.rqmnh6z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+  await mongoose
+    .connect(
+      "mongodb+srv://sohaibsipra869:nvidia940MX@cluster0.dcrk8mp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => {
+      console.log("Connected to database");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 export default connectToDb;
