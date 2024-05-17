e7c58766fa43c950a4c3c8c30bae698d0d94c4ae
Sohaib Ahmed <118601382+Sohaib-Ahmed869@users.noreply.github.com>
2024-05-17 17:29:22 +0500

    Update db.js

--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -1,14 +1,28 @@
-// utils/db.js
 import mongoose from "mongoose";
 
 async function connectToDb() {
   if (mongoose.connections[0].readyState) return; // Use existing database connection if it exists
-  // Connect to MongoDB
+  
+  // Connection options
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 45000, // 45 seconds
+  };
 
+  // Connect to MongoDB
   await mongoose
     .connect(
-      "mongodb+srv://sohaibsipra869:nvidia940MX@cluster0.dcrk8mp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://sohaibsipra869:nvidia940MX@cluster0.dcrk8mp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      options
     )
+    .then(() => {
+      console.log("Connected to database");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 export default connectToDb;
